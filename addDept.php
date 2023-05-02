51d8443ab1b8fc435adf1842e1cb48f3c1d2807d
Reem Khider <74662297+Reem-313@users.noreply.github.com>
2023-05-02 10:50:16 +0100

    enable user to add department

---
+++ addDept.php
@@ -0,0 +1,35 @@
+<?php 
+/** Connection with postgresql **/
+/** Connection with mysql **/
+$connection = new PDO("mysql:host=localhost;dbname=emloyeedb", 'root', '');  
+/** Preparation and execution of the query **/
+
+if(isset($_POST['submit']))
+{
+    $DeptName= $_POST['DeptName'];
+    $location= $_POST['location'];
+    $sql="INSERT into Dept (DNAME, LOC) VALUES ('$DeptName', '$location')";
+    $resultset = $connection->prepare($sql);
+    $resultset->execute();
+}
+/** Disconnection **/
+$connection=null;
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Music School</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+    <form method="post" action="" autocomplete="off">
+        <label for="DeptName">Department:</label><br>
+        <input type="text" id="DeptName" name="DeptName" required value=""><br>
+        <label for="location">last name:</label><br>
+        <input type="text" id="location" name="location" required value=""><br>
+        <input type="submit" name="submit"><br>
+    </form>
+    </body>
+</head>
+</html>
+
