72f6c15bcdb279fd568711355d76b237045e7b7b
Reem Khider <74662297+Reem-313@users.noreply.github.com>
2023-05-05 14:40:56 +0100

    implemented the insertion

--- addEmp.php
+++ addEmp.php
@@ -2,20 +2,30 @@
 /** Connection with postgresql **/
 /** Connection with mysql **/
 //https://stackoverflow.com/questions/6882633/php-get-input-radio-selection-data-and-insert-into-mysql-table
-$connection = new PDO("mysql:host=localhost;dbname=emloyeedb", 'root', '');  
+$connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
 /** Preparation and execution of the query **/
 
 if(isset($_POST['submit']))
 {
     $Ename= $_POST['ENAME'];
+   // $addressRoad= $_POST['addRoad'];
+   // $addressCity= $_POST['addCity'];
+   $selectionADD = $_POST['selectionAdd'];
     $selectionJob = $_POST['selectionJOB'];
     $HireDate= $_POST['HIREDATE'];
     $Salary= $_POST['SAL'];
     $commision= $_POST['COMM'];
     $selectionDept = $_POST['selectionDEPT'];
     $selectionMgr = $_POST['selectionMGR'];
-    $sql="INSERT into emp (ENAME, JOBID_FK, MGR, HIREDATE, SAL, COMM, DEPTNO_FK) VALUES 
-    ('$Ename', '$selectionJob', '$selectionMgr' ,'$HireDate', '$Salary', '$commision', '$selectionDept')";
+    $selectioncar = $_POST['selectionCar'];
+    if($selectioncar='')
+    {
+        
+    }
+
+
+    $sql="INSERT into employee (ENAME, MGR, HIREDATE, Salary, Commision, DeptID, JobID, addressID, carID) VALUES 
+    ('$Ename', '$selectionMgr' ,'$HireDate', '$Salary', '$commision', '$selectionDept', '$selectionJob', '$selectionADD', '$selectioncar')";
     $resultset = $connection->prepare($sql);
     $resultset->execute();
 }
@@ -32,10 +42,31 @@ $connection=null;
     <form method="post" action="" autocomplete="off">
         <label for="ENAME">Employee Name:</label><br>
         <input type="text" id="ENAME" name="ENAME"><br>
+        <label>Employee Address:</label><br>
+
+        <select name="selectionAdd">
+        <?php
+        $connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
+            $sql="SELECT * FROM address";
+            $resultset = $connection->prepare($sql);
+            $resultset->execute();   
+            $rows = $resultset->fetchAll(PDO::FETCH_ASSOC);
+            foreach($rows as $row)
+            {
+                ?>
+                <option value="<?php echo $row['addressID']; ?>"><?php echo $row['City']; ?></option>
+                <?php
+
+            }
+            ?>
+            </select>
+        <br>
+       
+
         <label for="JOBID">Employee Job:</label><br>
         <select name="selectionJOB">
         <?php
-        $connection = new PDO("mysql:host=localhost;dbname=emloyeedb", 'root', '');  
+        $connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
             $sql="SELECT * FROM job";
             $resultset = $connection->prepare($sql);
             $resultset->execute();   
@@ -43,7 +74,7 @@ $connection=null;
             foreach($rows as $row)
             {
                 ?>
-                <option value="<?php echo $row['JOBID']; ?>"><?php echo $row['JOBNAME']; ?></option>
+                <option value="<?php echo $row['JOBID']; ?>"><?php echo $row['JOBTitle']; ?></option>
                 <?php
 
             }
@@ -54,15 +85,15 @@ $connection=null;
         <select name="selectionMGR">
 
         <?php
-        $connection = new PDO("mysql:host=localhost;dbname=emloyeedb", 'root', '');  
-            $sql="SELECT * FROM emp";
+        $connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
+            $sql="SELECT * FROM employee";
             $resultset = $connection->prepare($sql);
             $resultset->execute();   
             $rows = $resultset->fetchAll(PDO::FETCH_ASSOC);
             foreach($rows as $row)
             {
                 ?>
-                <option value="<?php echo $row['EMPNO']; ?>"><?php echo $row['ENAME']; ?></option>
+                <option value="<?php echo $row['EmployeeID']; ?>"><?php echo $row['ENAME']; ?></option>
                 <?php
 
             }
@@ -83,7 +114,7 @@ $connection=null;
             <select name="selectionDEPT">
 
             <?php
-            $connection = new PDO("mysql:host=localhost;dbname=emloyeedb", 'root', '');  
+            $connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
                 $sql="SELECT * FROM dept";
                 $resultset = $connection->prepare($sql);
                 $resultset->execute();   
@@ -92,6 +123,27 @@ $connection=null;
                 {
                     ?>
                     <option value="<?php echo $row['DEPTNO']; ?>"><?php echo $row['DNAME']; ?></option>
+                    <?php
+
+                }
+                ?>
+                </select>
+                <br>
+
+                <label for="car">Do you want to assign the employee a car?</label><br>
+            <select name="selectionCar">
+            <option value=''>NULL</option>
+
+            <?php
+            $connection = new PDO("mysql:host=localhost;dbname=empdb", 'root', '');  
+                $sql="SELECT * FROM car";
+                $resultset = $connection->prepare($sql);
+                $resultset->execute();   
+                $rows = $resultset->fetchAll(PDO::FETCH_ASSOC);
+                foreach($rows as $row)
+                {
+                    ?>
+                    <option value="<?php echo $row['CarID']; ?>"> <?php echo $row['carMake']; ?> <?php echo $row['carModel']; ?></option>
                     <?php
 
                 }
